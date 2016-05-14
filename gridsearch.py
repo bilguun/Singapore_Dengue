62d34034c912369e43d34d94678d6106c120852a
chizzinho <lc3383@nyu.edu>
2016-05-14 11:43:48 -0400

    Clean repository

--- gridsearch.py
+++ gridsearch.py
@@ -1,7 +1,8 @@
-def get_results(n_components,max_features,criterion,n_estimators):
+def get_results(max_features,criterion,n_estimators,X,Y):
     from sklearn.metrics import accuracy_score
     from sklearn.cross_validation import KFold
     from sklearn.ensemble import RandomForestClassifier
+    import itertools
     kf = KFold(n=len(X), n_folds=12, shuffle=True, random_state=21)
     d = []
     for train_index, test_index in kf:
@@ -22,9 +23,4 @@ def get_results(n_components,max_features,criterion,n_estimators):
             forest_pred = forest.predict(X_validation)
             name = '%s,%s,%s'%(str(rf_features),str(criter),str(rf_estimators))
             d.append((name,accuracy_score(Y_validation,forest_pred),X_train,X_test,Y_train,Y_test))
-            #forest_probs = forest.predict_proba(X_test)
-            #sig_clf = CalibratedClassifierCV(forest, method="sigmoid", cv="prefit")
-            #sig_clf.fit(X_validation, Y_validation)
-            #sig_clf_probs = sig_clf.predict_proba(X_test)
-            #sig_score = log_loss(Y_test, sig_clf_probs)
     return sorted(d, key=lambda tup: tup[1])[-1]
